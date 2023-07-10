48cac85215ab4bb8083b1eefc426822bedc28642
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-07-10 09:26:49 -0400

    changed something

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 import { onRequest } from "firebase-functions/v2/https";
-import { express } from "express";
+import express from "express";
 import cors from "cors"
 import { getTasks, addTask } from "./src/tasks.js";
 
 const app = express();
 app.use(cors());
-app.use(express())
+app.use(express());
 
 //routes:
 app.get("/tasks/:uid", getTasks);
-app.post("/tasks/:uid" , addTask);
+app.post("/tasks/:uid", addTask);
 
 export const api = onRequest(app); // exports the cloud function
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,26 +1,30 @@
 import { FieldValue } from "firebase-admin/firestore";
 import dbConnect from "./dbConnect.js";
 
-const coll = dbConnect.collection("tasks")
+const coll = dbConnect.collection( "tasks" )
 
-export async function getTasks (req, res) {
+export async function getTasks(req, res) {
   const { uid } = req.params;
-  //get all tasks by user
-   const tasks = await coll.where('uid', '==', uid).get();
-   // arrange taks in an array
-   const taskArray = tasks.docs.map (doc => ({id: doc.id, ...doc.data() }))
-   res.send(taskArray);
-} 
+  // get all tasks by user:
+  const tasks = await coll.where('uid', '==', uid).get();
+  // arrange tasks in an array:
+  const taskArray = tasks.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  res.send(taskArray);
+}
 
-export async function addTask ( req, res) {
-  const {title, uid} = req.body;
+export async function addTask(req, res) {
+  const { title, uid } = req.body;
   if(!title || !uid) {
-  res.status(401).send({success: false, message: 'not a valid request'})
-  return;
+    res.status(401).send({ success: false, message: 'Not a valid request' });
+    return;
   }
   const newTask = {
-    title, uid , done: false,
+    title,
+    uid,
+    done: false,
     createdAt: FieldValue.serverTimestamp(),
   }
   await coll.add(newTask);
+  getTasks(req, res);
 }
+
